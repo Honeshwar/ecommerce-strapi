d0cedf5b5350fb16b7ae1d5658fd25334bdf2d23
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-10-05 03:10:07 +0000

    order problem reslove

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,5 +1,5 @@
 ("use strict");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 /**
  * order controller
  */
@@ -15,7 +15,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
           const item = await strapi
             .service("api::product.product")
             .findOne(product.id);
-            // console.log( process.env.CLIENT_URL+product.img);
+          console.log(process.env.CLIENT_URL);
 
           return {
             price_data: {
@@ -34,20 +34,21 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       );
 
       const session = await stripe.checkout.sessions.create({
-        shipping_address_collection: {allowed_countries: ['IN','US', 'CA']},
+        shipping_address_collection: { allowed_countries: ["IN", "US", "CA"] },
         payment_method_types: ["card"],
         mode: "payment",
-        success_url: process.env.CLIENT_URL+"?success=true",
-        cancel_url: process.env.CLIENT_URL+"?success=false",
+        success_url: process.env.CLIENT_URL + "?success=true",
+        cancel_url: process.env.CLIENT_URL + "?success=false",
         line_items: lineItems,
       });
 
       await strapi
         .service("api::order.order")
-        .create({ data: {  products, stripeId: session.id } });
+        .create({ data: { products, stripeId: session.id } });
 
       return { stripeSession: session };
     } catch (error) {
+      console.log("order controller", error);
       ctx.response.status = 500;
       return { error };
     }
