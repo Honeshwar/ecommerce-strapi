5d86616748d78b04d4147e11145f7ec06cd9b895
Honeshwar Thakur <st566654@gmail.com>
2026-01-26 13:36:05 +0530

    checking auto deployment delete product

--- scripts for strapi APIs/addProduct.js
+++ scripts for strapi APIs/addProduct.js
@@ -54,148 +54,145 @@ async function addProduct(product) {
 }
 
 // Example product list
-// const products = [
-//   {
-//     title: "Classic White T-Shirt",
-//     desc: "Soft cotton regular fit tee",
-//     price: 999,
-//     oldPrice: 1299,
-//     isNew: true,
-//     type: "trending",
-//     categories: [3], // Men
-//     sub_categories: [1], // T-Shirts,
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1704702039/indian_man_simple_white_tee_studio_portrait_de169cdc72.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853222/medium_tshirt_8726716_1280_7f7de55683.jpg"
-//     ]
-//   },
-//   {
-//     title: "Denim Jacket",
-//     desc: "Blue washed slim fit denim jacket",
-//     price: 2499,
-//     oldPrice: 2999,
-//     isNew: false,
-//     type: "trending",
-//     categories: [2], // Women
-//     sub_categories: [2], // Jackets
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853868/blue_2566082_1280_ddacd2694f.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853846/people_2589566_1280_0950d1d4e2.jpg"
-//     ]
-//   },
-//   {
-//     title: "Leather Handbag",
-//     desc: "Brown premium leather handbag",
-//     price: 3499,
-//     oldPrice: 3999,
-//     isNew: true,
-//     type: "featured",
-//     categories: [5], // Accessories
-//     sub_categories: [5], // Bags
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854328/ai_generated_8571263_1280_c9ef287031.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854327/handbag_8310851_1280_cae97a2488.jpg"
-//     ]
-//   },
-//   {
-//     title: "Running Sneakers",
-//     desc: "Lightweight breathable sneakers",
-//     price: 2999,
-//     oldPrice: 3499,
-//     isNew: true,
-//     type: "normal",
-//     categories: [4], // New Season
-//     sub_categories: [4], // Sneakers
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700928/medium_omar_prestwich_j_LE_Gurep_Dco_unsplash_d1877a6164.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700921/joseph_barrientos_4q_Sb_F_Wh_H_Ks_unsplash_54608ea19c.jpg"
-//     ]
-//   },
-//   {
-//     title: "Formal Pants",
-//     desc: "Black slim-fit trousers",
-//     price: 1999,
-//     oldPrice: 2499,
-//     isNew: false,
-//     type: "trending",
-//     categories: [3], // Men
-//     sub_categories: [3], // Pants
-//     images: [
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700917/large_portrait_happy_smiling_young_businessman_brown_suit_isolated_white_wall_326271be61.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700928/large_portrait_happy_smiling_young_businessman_blue_suit_isolated_white_wall_79a92f80c3.jpg"
-//     ]
-//   },
-//   {
-//     title: "Printed Maxi Dress",
-//     desc: "Floral summer maxi dress",
-//     price: 2299,
-//     oldPrice: 2699,
-//     isNew: true,
-//     type: "trending",
-//     categories: [2], // Women
-//     sub_categories: [1], // T-Shirts
-
-//   },
-//   {
-//     title: "Denim Overcoat",
-//     desc: "Warm denim overcoat",
-//     price: 3999,
-//     oldPrice: 4499,
-//     isNew: false,
-//     type: "trending",
-//     categories: [4], // New Season
-//     sub_categories: [2], // Jackets
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853868/blue_2566082_1280_ddacd2694f.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853846/people_2589566_1280_0950d1d4e2.jpg"
-//     ]
-//   },
-//   {
-//     title: "Casual Backpack",
-//     desc: "Durable canvas backpack",
-//     price: 1599,
-//     oldPrice: 1899,
-//     isNew: true,
-//     type: "featured",
-//     categories: [1], // Sales
-//     sub_categories: [5], // Bags
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757855700/woman_7405691_1280_87db98ca47.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757855699/medium_man_8192663_1280_93e8a33159.jpg"
-//     ]
-//   },
-//   {
-//     title: "Sports Sneakers",
-//     desc: "High grip sports shoes",
-//     price: 2799,
-//     oldPrice: 3199,
-//     isNew: true,
-//     type: "normal",
-//     categories: [6], // Shoes
-//     sub_categories: [4], // Sneakers
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854044/medium_nike_5020612_1280_1537819935.jpg",
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854045/small_shoes_3780166_1280_6c7735a6b2.jpg"
-//     ]
-//   },
-//   {
-//     title: "Evening Clutch",
-//     desc: "Elegant black party clutch",
-//     price: 1899,
-//     oldPrice: 2199,
-//     isNew: false,
-//     type: "featured",
-//     categories: [2], // Women
-//     sub_categories: [5], // Bags
-//     images:[
-//       "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700912/large_full_shot_woman_online_fashion_shopping_c93442f396.jpg",
-//       ""
-//     ]
-//   },
-// ];
-
 const products = [
   {
+    title: "Classic White T-Shirt",
+    desc: "Soft cotton regular fit tee",
+    price: 999,
+    oldPrice: 1299,
+    isNew: true,
+    type: "trending",
+    categories: [3], // Men
+    sub_categories: [1], // T-Shirts,
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1704702039/indian_man_simple_white_tee_studio_portrait_de169cdc72.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853222/medium_tshirt_8726716_1280_7f7de55683.jpg"
+    ]
+  },
+  {
+    title: "Denim Jacket",
+    desc: "Blue washed slim fit denim jacket",
+    price: 2499,
+    oldPrice: 2999,
+    isNew: false,
+    type: "trending",
+    categories: [2], // Women
+    sub_categories: [2], // Jackets
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853868/blue_2566082_1280_ddacd2694f.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853846/people_2589566_1280_0950d1d4e2.jpg"
+    ]
+  },
+  {
+    title: "Leather Handbag",
+    desc: "Brown premium leather handbag",
+    price: 3499,
+    oldPrice: 3999,
+    isNew: true,
+    type: "featured",
+    categories: [5], // Accessories
+    sub_categories: [5], // Bags
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854328/ai_generated_8571263_1280_c9ef287031.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854327/handbag_8310851_1280_cae97a2488.jpg"
+    ]
+  },
+  {
+    title: "Running Sneakers",
+    desc: "Lightweight breathable sneakers",
+    price: 2999,
+    oldPrice: 3499,
+    isNew: true,
+    type: "normal",
+    categories: [4], // New Season
+    sub_categories: [4], // Sneakers
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700928/medium_omar_prestwich_j_LE_Gurep_Dco_unsplash_d1877a6164.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700921/joseph_barrientos_4q_Sb_F_Wh_H_Ks_unsplash_54608ea19c.jpg"
+    ]
+  },
+  {
+    title: "Formal Pants",
+    desc: "Black slim-fit trousers",
+    price: 1999,
+    oldPrice: 2499,
+    isNew: false,
+    type: "trending",
+    categories: [3], // Men
+    sub_categories: [3], // Pants
+    images: [
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700917/large_portrait_happy_smiling_young_businessman_brown_suit_isolated_white_wall_326271be61.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700928/large_portrait_happy_smiling_young_businessman_blue_suit_isolated_white_wall_79a92f80c3.jpg"
+    ]
+  },
+  {
+    title: "Printed Maxi Dress",
+    desc: "Floral summer maxi dress",
+    price: 2299,
+    oldPrice: 2699,
+    isNew: true,
+    type: "trending",
+    categories: [2], // Women
+    sub_categories: [1], // T-Shirts
+
+  },
+  {
+    title: "Denim Overcoat",
+    desc: "Warm denim overcoat",
+    price: 3999,
+    oldPrice: 4499,
+    isNew: false,
+    type: "trending",
+    categories: [4], // New Season
+    sub_categories: [2], // Jackets
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853868/blue_2566082_1280_ddacd2694f.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757853846/people_2589566_1280_0950d1d4e2.jpg"
+    ]
+  },
+  {
+    title: "Casual Backpack",
+    desc: "Durable canvas backpack",
+    price: 1599,
+    oldPrice: 1899,
+    isNew: true,
+    type: "featured",
+    categories: [1], // Sales
+    sub_categories: [5], // Bags
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757855700/woman_7405691_1280_87db98ca47.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757855699/medium_man_8192663_1280_93e8a33159.jpg"
+    ]
+  },
+  {
+    title: "Sports Sneakers",
+    desc: "High grip sports shoes",
+    price: 2799,
+    oldPrice: 3199,
+    isNew: true,
+    type: "normal",
+    categories: [6], // Shoes
+    sub_categories: [4], // Sneakers
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854044/medium_nike_5020612_1280_1537819935.jpg",
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854045/small_shoes_3780166_1280_6c7735a6b2.jpg"
+    ]
+  },
+  {
+    title: "Evening Clutch",
+    desc: "Elegant black party clutch",
+    price: 1899,
+    oldPrice: 2199,
+    isNew: false,
+    type: "featured",
+    categories: [2], // Women
+    sub_categories: [5], // Bags
+    images:[
+      "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700912/large_full_shot_woman_online_fashion_shopping_c93442f396.jpg",
+      ""
+    ]
+  },
+   {
     title: "Classic White T-Shirt",
     desc: "This classic white t-shirt is crafted from premium soft cotton fabric that feels gentle on the skin and stays comfortable all day long. Designed with a regular fit, it offers a clean and timeless look that works well for casual outings, office wear, or layering under jackets. The breathable material ensures proper airflow, making it ideal for warm weather. Easy to maintain and durable, this t-shirt retains its shape and color even after multiple washes, making it a reliable wardrobe essential.",
     price: 999,
@@ -317,24 +314,6 @@ const products = [
       "https://res.cloudinary.com/drct1kgjv/image/upload/v1757854045/small_shoes_3780166_1280_6c7735a6b2.jpg"
     ]
   },
-  // {
-  //   title: "Evening Clutch",
-  //   desc: "This elegant evening clutch is designed to complement your party and formal outfits. Featuring a sleek black finish, it adds a refined touch to your overall look. The compact interior provides enough space for essentials like phone, cards, and makeup. Lightweight and easy to carry, it is perfect for weddings, parties, and special occasions. Crafted with quality materials, this clutch offers durability while maintaining a stylish and luxurious appearance.",
-  //   price: 1899,
-  //   oldPrice: 2199,
-  //   isNew: false,
-  //   type: "featured",
-  //   categories: [2],
-  //   sub_categories: [5],
-  //   images: [
-  //     "https://res.cloudinary.com/drct1kgjv/image/upload/v1704700912/large_full_shot_woman_online_fashion_shopping_c93442f396.jpg",
-  //     ""
-  //   ]
-  // }
-];
-
-// image i add in new pic folder
-const productss = [
   {
     title: "Oversized Graphic T-Shirt",
     desc: "This oversized graphic t-shirt is designed for everyday comfort and modern street style. Made from breathable cotton fabric, it feels soft on the skin and allows free movement throughout the day. The relaxed fit gives it a trendy look that pairs perfectly with jeans or joggers. Durable stitching ensures long-lasting wear, while the bold graphic print adds a fashionable edge. Ideal for casual outings, travel, or lounging, this t-shirt is a versatile addition to your wardrobe.",
@@ -422,8 +401,8 @@ const productss = [
 ];
 
 
-// (async () => {
+(async () => {
   for (let product of products) {
     await addProduct(product);
   }
-// })();
+})();
--- stapi-cloud-deployment.md
+++ stapi-cloud-deployment.md
@@ -1,3 +1,7 @@
 # Deployment
 it not need any cloudinary, postgres, because it store internally(500db entries) enough
 - it take .env file in root also no need to add from dashboard of cloud.strapi.com
+
+## Issue: 
+token of stripe not able to pass in headers, throw error by stripe
+solution: delete old deployment and instead of adding env from dashboard i add .env file at root of project and its work
